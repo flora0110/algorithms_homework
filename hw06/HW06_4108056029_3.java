4acbd77a738282b023a063aa9d3a2db76075d4bf
flora <flora90.1.10@gmail.com>
2021-04-13 01:11:28 +0800

    +a +threads no debug

--- hw06/HW06_4108056029_3.java
+++ hw06/HW06_4108056029_3.java
@@ -2,43 +2,81 @@ public class HW06_4108056029_3 extends Dessert_Desert{
     int[][] inputArr;
     int len;//each array's length
     public int[] maxBlocks(int[][] inputArr){
+        int num_thread = 8,lgTNum=3;
         int n=inputArr.length;
         this.inputArr = inputArr;
         int[] ans = new int[n];
-        int i,j,k,temp;
-        int[] dif;
-        for(i=0;i<n;i++){
-            len = inputArr[i].length;
-            dif = new int[len];
-            for(j=0;j<len;j++){
-                dif[j]=1;
-                for(k=j;k>0;k--){
-                    if(inputArr[i][k]<inputArr[i][k-1]){
-                        temp = inputArr[i][k];
-                        inputArr[i][k] = inputArr[i][k-1];
-                        inputArr[i][k-1] = temp;
-                        dif[k]=j-k+2;
-                        dif[k-1]=j-k+2;
-                    }
-                    else break;
+        Thread[] t = new Thread[num_thread];
+        if(n<num_thread){
+            System.out.println("in");
+            int i,j;
+            int[] max;
+            int[] min;
+            int a;
+            for(i=0;i<n;i++){
+                a=1;
+                len = inputArr[i].length;
+                max = new int[len];
+                min = new int[len];
+                max[0] = inputArr[i][0];
+                min[len-1] = inputArr[i][len-1];
+                for(j=len-2;j>=0;j--){
+                    min[j] = Math.min(min[j+1],inputArr[i][j]);
                 }
-                show(dif);
+                for(j=1;j<len;j++){
+                    max[j] = Math.max(max[j-1],inputArr[i][j]);
+                    if(max[j-1]<=min[j]){
+                        a++;
+                    }
+                }
+                ans[i]=a;
+                System.out.println(ans[i]);
             }
-            for(j=0;j<len;){
-                System.out.println("j= "+j);
-                ans[i]++;
-                j+=dif[j];
-            }
-            System.out.println(ans[i]);
         }
-        return ans;
-    }
-    public void show(int[] dif){
-        //System.out.println();
-        for(int i=0;i<dif.length;i++){
-            System.out.print(dif[i]+" ");
+        else{
+            System.out.println("else");
+            for(int control = 0;control<num_thread;control++){
+                final int k=control;
+                t[control] = new Thread(new Runnable(){
+                    public void run(){
+                        int[] max;
+                        int[] min;
+                        int a;
+                        final int start = (n*k) >>lgTNum;
+                        final int end = (n*(k+1))>>lgTNum;
+                        for(int i=start;i<end;i++){
+                            a=1;
+                            len = inputArr[i].length;
+                            max = new int[len];
+                            min = new int[len];
+                            max[0] = inputArr[i][0];
+                            min[len-1] = inputArr[i][len-1];
+                            for(int j=len-2;j>=0;j--){
+                                min[j] = Math.min(min[j+1],inputArr[i][j]);
+                            }
+                            for(int j=1;j<len;j++){
+                                max[j] = Math.max(max[j-1],inputArr[i][j]);
+                                if(max[j-1]<=min[j]){
+                                    a++;
+                                }
+                            }
+                            ans[i]=a;
+                        }
+                    }
+                });
+                t[control].start();
+            }
+            try{
+                for(int control=0; control<num_thread; control++){
+                    t[control].join();
+                }
+            }catch(InterruptedException e){}
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(ans[i]+" ");
         }
         System.out.println();
+        return ans;
     }
     public static void main(String[] args){
         HW06_4108056029_3 test = new HW06_4108056029_3();
@@ -49,7 +87,16 @@ public class HW06_4108056029_3 extends Dessert_Desert{
                         {1, 2, 3},
                         {5, 4, 3, 2, 1},
                         {2, 1, 3, 2},
-                        {2,5,4,1,2}};
+                        {2,5,4,1,2},
+                        {1, 3, 2, 5, 4, 7, 6},
+                        {1, 1, 1, 1, 1, 1, 1},
+                        {1, 3, 5, 7, 9},
+                        {1, 2, 3},
+                        {5, 4, 3, 2, 1},
+                        {2, 1, 3, 2},
+                        {2,5,4,1,2},
+
+                    };
         test.maxBlocks(arr);
     }
 }
