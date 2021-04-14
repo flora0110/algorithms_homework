be321f8c38a1af173204f4e0edaee4b0a6082557
flora <flora90.1.10@gmail.com>
2021-04-14 17:46:32 +0800

    4thread error

--- hw06/HW06_4108056029_3.java
+++ hw06/HW06_4108056029_3.java
@@ -1,81 +1,89 @@
 public class HW06_4108056029_3 extends Dessert_Desert{
     int[][] inputArr;
-    int len;//each array's length
     public int[] maxBlocks(int[][] inputArr){
-        int num_thread = 8,lgTNum=3;
+        int num_thread = 4,lgTNum=2;
         int n=inputArr.length;
         this.inputArr = inputArr;
         int[] ans = new int[n];
         Thread[] t = new Thread[num_thread];
         if(n<num_thread){
-            System.out.println("in");
-            int i,j;
-            int[] max;
-            int[] min;
-            int a;
+            int i,j,len,max;
+            int[] min = new int[1000000];
             for(i=0;i<n;i++){
-                a=1;
                 len = inputArr[i].length;
-                max = new int[len];
-                min = new int[len];
-                max[0] = inputArr[i][0];
+                max = inputArr[i][0];
                 min[len-1] = inputArr[i][len-1];
                 for(j=len-2;j>=0;j--){
-                    min[j] = Math.min(min[j+1],inputArr[i][j]);
+                    if(inputArr[i][j] < min[j+1]) min[j] = inputArr[i][j];
+                    else min[j] = min[j+1];
                 }
-                for(j=1;j<len;j++){
-                    max[j] = Math.max(max[j-1],inputArr[i][j]);
-                    if(max[j-1]<=min[j]){
-                        a++;
+                for(j=0;j<len-1;j++){
+                    if(inputArr[i][j]>max) max = inputArr[i][j];
+                    if(max<=min[j+1]){
+                        ans[i]++;
                     }
                 }
-                ans[i]=a;
-                System.out.println(ans[i]);
             }
         }
         else{
-            System.out.println("else");
-            for(int control = 0;control<num_thread;control++){
+            for(int control = 0;control<num_thread-1;control++){
                 final int k=control;
                 t[control] = new Thread(new Runnable(){
                     public void run(){
-                        int[] max;
-                        int[] min;
-                        int a;
+                        int[] min = new int[1000000];
+                        int len,max;
                         final int start = (n*k) >>lgTNum;
                         final int end = (n*(k+1))>>lgTNum;
                         for(int i=start;i<end;i++){
-                            a=1;
                             len = inputArr[i].length;
-                            max = new int[len];
-                            min = new int[len];
-                            max[0] = inputArr[i][0];
+                            max = inputArr[i][0];
                             min[len-1] = inputArr[i][len-1];
                             for(int j=len-2;j>=0;j--){
-                                min[j] = Math.min(min[j+1],inputArr[i][j]);
+                                if(inputArr[i][j] < min[j+1]) min[j] = inputArr[i][j];
+                                else min[j] = min[j+1];
                             }
-                            for(int j=1;j<len;j++){
-                                max[j] = Math.max(max[j-1],inputArr[i][j]);
-                                if(max[j-1]<=min[j]){
-                                    a++;
+                            for(int j=0;j<len-1;j++){
+                                if(inputArr[i][j]>max) max = inputArr[i][j];
+                                if(max<=min[j+1]){
+                                    ans[i]++;
                                 }
                             }
-                            ans[i]=a;
                         }
                     }
                 });
                 t[control].start();
+
             }
+            t[num_thread-1] = new Thread(new Runnable(){
+                public void run(){
+                    int[] min = new int[1000000];
+                    int a,len,max;
+                    int start = (n*(num_thread-1)) >>lgTNum;
+                    for(int i=start;i<n;i++){
+                        a=1;
+                        len = inputArr[i].length;
+                        max = inputArr[i][0];
+                        min[len-1] = inputArr[i][len-1];
+                        for(int j=len-2;j>=0;j--){
+                            if(inputArr[i][j] < min[j+1]) min[j] = inputArr[i][j];
+                            else min[j] = min[j+1];
+                        }
+                        for(int j=0;j<len-1;j++){
+                            if(inputArr[i][j]>max) max = inputArr[i][j];
+                            if(max<=min[j+1]){
+                                ans[i]++;
+                            }
+                        }
+                    }
+                }
+            });
+            t[num_thread-1].start();
             try{
                 for(int control=0; control<num_thread; control++){
                     t[control].join();
                 }
             }catch(InterruptedException e){}
         }
-        for(int i=0;i<n;i++){
-            System.out.print(ans[i]+" ");
-        }
-        System.out.println();
         return ans;
     }
     public static void main(String[] args){
