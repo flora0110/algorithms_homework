9876a3db96cfeb6e2a1f931aa9961cb623155a20
flora <flora90.1.10@gmail.com>
2021-04-26 05:01:52 +0800

    debug best phone

--- hw07/HW07_4108056029_4.java
+++ hw07/HW07_4108056029_4.java
@@ -10,36 +10,40 @@ public class HW07_4108056029_4 extends Buy_Phone{
         //linklist array = new linklist(-1,-1);
         //linklist(-1,-1);
         for(i=n-1;i>=0;i--){
-            System.out.println(inputArr[i][0]+" "+inputArr[i][1]);
+            //System.out.println(inputArr[i][0]+" "+inputArr[i][1]);
             add(inputArr[i][0],inputArr[i][1]);
         }
-        add(-1,-1);
-        System.out.println("first"+first.x+" "+first.y);
-        int num=n,maxy;
-        Node last = first,last_com;
+        add(-1,-1);//title
+        //System.out.println("first"+first.x+" "+first.y);
+        int num=0,maxy;
+        Node last = first;
         Node temp = last.next;
         Node compare;
         while(temp!=null){
-            System.out.println("temp "+temp.x+" "+temp.y);
-            compare = temp.next;
-            maxy = temp.y;
+            num++;
+            compare = temp.next;//start to compare from temp's next to end
+            maxy = temp.y;//remind the max y
             while(compare!=null){
-                last_com = last;
+                //last_com = last;//compare's last
                 System.out.println("compare "+compare.x+" "+compare.y);
-                if(compare.y > maxy){
-                    temp = last;
+                if(compare.y >= maxy){//sofar y is max && x is max
+                    //temp = last;
                     //System.out.println("last "+last.x+" "+last.y+" maxy "+maxy);
-                    last_com.next = last_com.next.next;
-                    maxy = compare.y;
+                    System.out.println("before delete last "+last.x+" "+last.y);
+                    last.next = compare;//delete start to now
+                    //System.out.println("last_com.next "+last_com.next.x+" "+last_com.next.y);
+                    maxy = compare.y;//update maxy
                     //temp = last.next;//
                     //System.out.println("change temp "+temp.x+" "+temp.y);
-                    num--;
+                    //num--;
                 }
-                last_com = last_com.next;
+                //last_com = last_com.next;
+                //System.out.println("last_com "+last_com.x+" "+last_com.y);
                 compare = compare.next;
             }
-            last = temp;
-            temp = last.next;
+            temp = last.next.next;
+            last = last.next;
+
         }
 
 
@@ -110,9 +114,10 @@ public class HW07_4108056029_4 extends Buy_Phone{
     }
     public static void main(String[] args){
         HW07_4108056029_4 test = new HW07_4108056029_4();
-        int[][] inputArr = {{2,4},{2,10},{5,4},{4,8},{1,1},{5,5},{8,4},{10,2},{10,1}};
+        //int[][] inputArr = {{2,4},{2,10},{5,4},{4,8},{1,1},{5,5},{8,4},{10,2},{10,1}};
         //int[][] inputArr = {{2,1},{5,3},{5,4},{7,2}};
-        //int[][] inputArr = {{2,4},{2,5},{5,3},{5,4},{7,6}};
+        int[][] inputArr = {{2,4},{2,5},{5,3},{5,4},{7,6}};
+        //int[][] inputArr = {{0,10},{1,1},{2,3},{2,4},{3,1},{4,6},{4,8},{5,1},{6,4}};
         int[][] ans= test.bestPhone(inputArr);
         System.out.println("ans");
         for(int i=0;i<ans.length;i++){
