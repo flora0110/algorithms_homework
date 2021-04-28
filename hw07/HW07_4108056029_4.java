ac59240894863f262dc347c9a63bc22afa05e71f
flora <flora90.1.10@gmail.com>
2021-04-28 17:21:31 +0800

    linklist/ nocutoffquick

--- hw07/HW07_4108056029_4.java
+++ hw07/HW07_4108056029_4.java
@@ -25,11 +25,9 @@ public class HW07_4108056029_4 extends Buy_Phone{
             maxy = temp.y;//remind the max y
             while(compare!=null){
                 //last_com = last;//compare's last
-                System.out.println("compare "+compare.x+" "+compare.y);
                 if(compare.y >= maxy){//sofar y is max && x is max
                     //temp = last;
                     //System.out.println("last "+last.x+" "+last.y+" maxy "+maxy);
-                    System.out.println("before delete last "+last.x+" "+last.y);
                     last.next = compare;//delete start to now
                     //System.out.println("last_com.next "+last_com.next.x+" "+last_com.next.y);
                     maxy = compare.y;//update maxy
@@ -76,11 +74,27 @@ public class HW07_4108056029_4 extends Buy_Phone{
         }
     //}
     public void quick_sort(int left,int right){
+        int[] temp = new int[2];
         if(right<=left+9){
-            insertion_sort();
+            for(int i = 0; i < n; i++){
+                for (int j = i; j > 0; j--){
+                    if (inputArr[j][0]<inputArr[j-1][0]){
+                        temp = inputArr[j];
+                        inputArr[j] = inputArr[j-1];
+                        inputArr[j-1] = temp;
+                    }
+                    else if(inputArr[j][0]==inputArr[j-1][0] && inputArr[j][1]<inputArr[j-1][1]){
+                        temp = inputArr[j];
+                        inputArr[j] = inputArr[j-1];
+                        inputArr[j-1] = temp;
+                    }
+                    else break;
+                }
+            }
+            //insertion_sort();
             return;
         }
-        int[] temp = new int[2];
+        //int[] temp = new int[2];
         int[] s=inputArr[(left+right)>>1];
         int i=left,j=right;
         while(i<j) {
@@ -94,34 +108,19 @@ public class HW07_4108056029_4 extends Buy_Phone{
         quick_sort(left,i-1);
         quick_sort(j+1,right);
     }
-    public void insertion_sort(){
-        int[] temp = new int[2];
-        for(int i = 0; i < n; i++){
-            for (int j = i; j > 0; j--){
-                if (inputArr[j][0]<inputArr[j-1][0]){
-                    temp = inputArr[j];
-                    inputArr[j] = inputArr[j-1];
-                    inputArr[j-1] = temp;
-                }
-                else if(inputArr[j][0]==inputArr[j-1][0] && inputArr[j][1]<inputArr[j-1][1]){
-                    temp = inputArr[j];
-                    inputArr[j] = inputArr[j-1];
-                    inputArr[j-1] = temp;
-                }
-                else break;
-            }
-        }
-    }
+    /*public void insertion_sort(){
+
+    }*/
     public static void main(String[] args){
         HW07_4108056029_4 test = new HW07_4108056029_4();
-        //int[][] inputArr = {{2,4},{2,10},{5,4},{4,8},{1,1},{5,5},{8,4},{10,2},{10,1}};
+        int[][] inputArr = {{2,4},{2,10},{5,4},{4,8},{1,1},{5,5},{8,4},{10,2},{10,1}};
         //int[][] inputArr = {{2,1},{5,3},{5,4},{7,2}};
-        int[][] inputArr = {{2,4},{2,5},{5,3},{5,4},{7,6}};
+        //int[][] inputArr = {{2,4},{2,5},{5,3},{5,4},{7,6}};
         //int[][] inputArr = {{0,10},{1,1},{2,3},{2,4},{3,1},{4,6},{4,8},{5,1},{6,4}};
         int[][] ans= test.bestPhone(inputArr);
-        System.out.println("ans");
+        /*System.out.println("ans");
         for(int i=0;i<ans.length;i++){
             System.out.println(ans[i][0]+" "+ans[i][1]);
-        }
+        }*/
     }
 }
