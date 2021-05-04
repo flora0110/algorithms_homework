d8c22d12587d056d16df9a6c21667d9dcc307282
flora <flora90.1.10@gmail.com>
2021-05-04 12:34:51 +0800

    static array & static ans

--- hw08/HW08_4108056029_1.java
+++ hw08/HW08_4108056029_1.java
@@ -1,28 +1,31 @@
 public class HW08_4108056029_1 extends Buy_Phone_v2{
     //int[][] inputArr;
-    int n;
-    final static int[][] aux = new int[100000][2];
+    //int n;
+    static int[][] array;
+    final static public int[][] aux = new int[10000][2];
+    static int[][] ans;
     public int[][] bestPhone(int[][] inputArr){
-        n = inputArr.length;
-        //this.inputArr = inputArr;
-        sort(inputArr,0,n-1,0);
+        array = inputArr;
+        int n = array.length;
+        //this.array = array;
+        sort(array,0,n-1,0);
         int[][] exist = new int[n][6];
-        exist[0] = inputArr[n-1];
+        exist[0] = array[n-1];
         int num=1;//ans array's size
         int win;
         int check;
-        for(int i=n-2;i>=0;i--){//control inputArr[i]
+        for(int i=n-2;i>=0;i--){//control array[i]
             check=1;
             for(int j=0;j<num;j++){//control exist[j]
                 win=0;
                 for(int h=1;h<6;h++){
-                    if(inputArr[i][h]>exist[j][h]){
+                    if(array[i][h]>exist[j][h]){
                         win=1;
                     }
                 }
                 if(win==0){ check=0; break;}
             }
-            if(check==1) exist[num++] = inputArr[i];
+            if(check==1) exist[num++] = array[i];
         }
         int[][] ans = new int[num][6];
         int count = 0;
@@ -31,39 +34,39 @@ public class HW08_4108056029_1 extends Buy_Phone_v2{
         }
         return ans;
     }
-    public static void sort(int[][] inputArr,int lo,int hi,int d){//inputArr->x
+    private static void sort(int[][] array,int lo,int hi,int d){//array->x
         if(hi<=lo) return;
         int [] count = new int[12];
         int i;
         for(i=lo;i<=hi;i++){
-            count[inputArr[i][d]+2]++;
+            count[array[i][d]+2]++;
         }
         for(i=1;i<11;i++){
             count[i+1] += count[i];
         }
         for(i=lo;i<=hi;i++){
-            aux[count[inputArr[i][d]+1]++] = inputArr[i];
-            //auxmark[count[inputArr[i][d]+1]++] = mark[i];
+            aux[count[array[i][d]+1]++] = array[i];
+            //auxmark[count[array[i][d]+1]++] = mark[i];
         }
         for(i=lo;i<=hi;i++){
-            inputArr[i] = aux[i-lo];
+            array[i] = aux[i-lo];
             //mark[i] = auxmark[i-lo];
         }
         for(i=0;i<10;i++){
             if(d==3) d=-1;
-            sort(inputArr,lo+count[i],lo+count[i+1]-1,d+1);
+            sort(array,lo+count[i],lo+count[i+1]-1,d+1);
         }
     }
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         HW08_4108056029_1 test = new HW08_4108056029_1();
         //int[][] inputArr = {{2,3,5,0,1,2,3},{1,7,3,2,5,0,1},{3,0,0,2,3,4,7},{0,2,3,4,5,6,1},{2,2,5,6,7,1,0}};
         int[][] inputArr = {{8,7,7,4,2,1},{2,4,9,2,2,1},{4,0,5,1,3,2},{5,2,4,3,7,3},{7,5,6,9,8,9}};
         int[][] ans = test.bestPhone(inputArr);
-        /*for(int i=0;i<ans.length;i++){
+        for(int i=0;i<ans.length;i++){
             for(int j=0;j<6;j++){
                 System.out.print(ans[i][j]+" ");
             }
             System.out.println();
-        }*/
-    }
+        }
+    }*/
 }
--- hw08/HW08_4108056029_4.java
+++ hw08/HW08_4108056029_4.java
@@ -1,23 +1,26 @@
 public class HW08_4108056029_4 extends Buy_Phone_v2{
     //int[][] inputArr;
-    int n;
-    static int[][] aux = new int[10000][2];
+    static int[][] array;
+    //int n;
+    final static public int[][] aux = new int[10000][2];
+    static int[][] ans;
     public int[][] bestPhone(int[][] inputArr){
-        n = inputArr.length;
+        array = inputArr;
+        int n = array.length;
         //this.inputArr = inputArr;
-        sort(inputArr,0,n-1,0);
+        sort(array,0,n-1,0);
         /*for(int i=0;i<5;i++){
             for(int j=0;j<6;j++){
-                System.out.print(inputArr[i][j]+" ");
+                System.out.print(array[i][j]+" ");
             }
             System.out.println();
         }*/
         int check;//if win[] is all 1 : check=1 else check=0(can be replace)
-        node[] position = new node[6];//if inputArr[i] can remind , than at link header[h] it will after position[h]
+        node[] position = new node[6];//if array[i] can remind , than at link header[h] it will after position[h]
         node[] header = new node[6];//small to big
         int is_bigest;//if it is bigest at header[h],position[h] is the last node in header[h]
         for(int i=1;i<6;i++){
-            header[i] = new node(null,-1,null);//will point to inputArr[i] which has smallest bit at h
+            header[i] = new node(null,-1,null);//will point to array[i] which has smallest bit at h
             //position[i] = header[i];
         }
         node header0 = new node(null,-1,null);
@@ -25,7 +28,7 @@ public class HW08_4108056029_4 extends Buy_Phone_v2{
         int num=0;//ans array's size
         //System.out.println("before for loop");
         for(int i=n-1;i>=0;i--){
-            int[] win = new int[n];//win[n] = inputArr[i] win at some bit
+            int[] win = new int[n];//win[n] = array[i] win at some bit
             //System.out.println("i= "+i);
             for(int h=1;h<6;h++){
                 //System.out.println("h= "+h);
@@ -33,19 +36,19 @@ public class HW08_4108056029_4 extends Buy_Phone_v2{
                 is_bigest = 1;
                 while(now.next!=null){
                     //System.out.println("now.next id= "+now.next.id);
-                    if(inputArr[i][h]<=now.next.arr[h]){//if inputArr[i] cant win now.next.arr
+                    if(array[i][h]<=now.next.arr[h]){//if array[i] cant win now.next.arr
                         //System.out.println("in if");
                         position[h] = now;//last smaller than i
                         is_bigest=0;//isnt biggest
                         break;
                     }
-                    win[now.next.id] = 1;//inputArr[i] win now.next.arr
+                    win[now.next.id] = 1;//array[i] win now.next.arr
                     now = now.next;
                 }
                 if(is_bigest==1) position[h] = now;
                 //System.out.println("position "+h+"'s id"+ position[h].id);
             }
-            check=1;//if inputArr[i] didnt lose all bit with any inputArr
+            check=1;//if array[i] didnt lose all bit with any array
             now = header0;
             //System.out.println("header0's id : "+ header0.id);
             while(now.next!=null){
@@ -58,10 +61,10 @@ public class HW08_4108056029_4 extends Buy_Phone_v2{
             }
             if(check == 1){//cant be replace
                 for(int h=1;h<6;h++){//insert in header[]
-                    node new_node = new node(inputArr[i],i,position[h].next);
+                    node new_node = new node(array[i],i,position[h].next);
                     position[h].next = new_node;
                 }
-                node new_node = new node(inputArr[i],i,now.next);
+                node new_node = new node(array[i],i,now.next);
                 now.next = new_node;//add it in the last of header0
                 num++;//ans's number+1
             }
@@ -84,7 +87,7 @@ public class HW08_4108056029_4 extends Buy_Phone_v2{
             //test---------------------------------------------*/
         }
         //System.out.println("num: "+num);
-        int[][] ans = new int[num][6];
+        ans = new int[num][6];
         now = header0.next;
         for(int i=num-1;i>=0;i--){
             //System.out.println("now.id: "+now.id);
@@ -104,39 +107,39 @@ public class HW08_4108056029_4 extends Buy_Phone_v2{
             this.next = next;
         }
     }
-    public static void sort(int[][] inputArr,int lo,int hi,int d){//inputArr->x
+    private static void sort(int[][] array,int lo,int hi,int d){//array->x
         if(hi<=lo) return;
         int [] count = new int[12];
         int i;
         for(i=lo;i<=hi;i++){
-            count[inputArr[i][d]+2]++;
+            count[array[i][d]+2]++;
         }
         for(i=1;i<11;i++){
             count[i+1] += count[i];
         }
         for(i=lo;i<=hi;i++){
-            aux[count[inputArr[i][d]+1]++] = inputArr[i];
-            //auxmark[count[inputArr[i][d]+1]++] = mark[i];
+            aux[count[array[i][d]+1]++] = array[i];
+            //auxmark[count[array[i][d]+1]++] = mark[i];
         }
         for(i=lo;i<=hi;i++){
-            inputArr[i] = aux[i-lo];
+            array[i] = aux[i-lo];
             //mark[i] = auxmark[i-lo];
         }
         for(i=0;i<10;i++){
             if(d==3) d=-1;
-            sort(inputArr,lo+count[i],lo+count[i+1]-1,d+1);
+            sort(array,lo+count[i],lo+count[i+1]-1,d+1);
         }
     }
-    public static void main(String[] args){
-        HW08_4108056029_4 test = new HW08_4108056029_4();
-        //int[][] inputArr = {{2,3,5,0,1,2,3},{1,7,3,2,5,0,1},{3,0,0,2,3,4,7},{0,2,3,4,5,6,1},{2,2,5,6,7,1,0}};
-        int[][] inputArr = {{8,7,7,4,2,1},{2,4,9,2,2,1},{4,0,5,1,3,2},{5,2,4,3,7,3},{7,5,6,9,8,9},{8,1,1,1,1,1}};
-        int[][] ans = test.bestPhone(inputArr);
-        /*for(int i=0;i<ans.length;i++){
-            for(int j=0;j<6;j++){
-                System.out.print(ans[i][j]+" ");
-            }
-            System.out.println();
-        }*/
-    }
+    //public static void main(String[] args){
+    //    HW08_4108056029_4 test = new HW08_4108056029_4();
+    //    //int[][] inputArr = {{2,3,5,0,1,2,3},{1,7,3,2,5,0,1},{3,0,0,2,3,4,7},{0,2,3,4,5,6,1},{2,2,5,6,7,1,0}};
+    //    int[][] inputArr = {{8,7,7,4,2,1},{2,4,9,2,2,1},{4,0,5,1,3,2},{5,2,4,3,7,3},{7,5,6,9,8,9},{8,1,1,1,1,1}};
+    //    int[][] ans = test.bestPhone(inputArr);
+    //    for(int i=0;i<ans.length;i++){
+    //        for(int j=0;j<6;j++){
+    //            System.out.print(ans[i][j]+" ");
+    //        }
+    //        System.out.println();
+    //    }
+    //}
 }
