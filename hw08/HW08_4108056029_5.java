5b13adfb2d2d98a4bd753e66fdb024ea2ec55ed3
flora <flora90.1.10@gmail.com>
2021-05-05 21:57:30 +0800

    shell sort

--- hw08/HW08_4108056029_5.java
+++ hw08/HW08_4108056029_5.java
@@ -1,144 +1,137 @@
-//position error -> ok
-//win[] -> int win
-//header0 -> small to big new_node = new node(,,header0.next); header0.next = new_node;->no
-// tail for header0?->no
-//header0 node[]->int[]? ->ok
-//binary search to find?
-//arraycopy??
 public class HW08_4108056029_5 extends Buy_Phone_v2{
-    final static public int[][] aux = new int[10000][6];
     int[][] array;
-    //int n;
+    final static public int[][] aux = new int[10000][6];
+    //final static public int[][] aux = new int[10000][2];
+    static int[][] ans;
+    static int n;
     public int[][] bestPhone(int[][] inputArr){
-        //this.inputArr = inputArr;
+        array = new int[inputArr.length][6];
+        System.arraycopy(inputArr,0,array,0,inputArr.length);
         //array = inputArr;
-        int n = inputArr.length;
-        array = new int[n][6];
-        System.arraycopy(inputArr,0,array,0,n);
-
-        sort(array,0,n-1);
-        /*System.out.println("sort------------------------");
+        n = array.length;
+        //this.array = array;
+        sort(array);
+        /*System.out.println("---------------------------");
         for(int i=0;i<n;i++){
             for(int j=0;j<6;j++){
                 System.out.print(array[i][j]+" ");
             }
             System.out.println();
         }
-        System.out.println("----------------------------");*/
-        int check;//if win[] is all 1 : check=1 else check=0(can be replace)
-        node[] position = new node[6];//if array[i] can remind , than at link header[h] it will after position[h]
-        node[] header = new node[6];//small to big
-        int is_bigest;//if it is bigest at header[h],position[h] is the last node in header[h]
-        for(int i=1;i<6;i++){
-            header[i] = new node(null,-1,null);//will point to array[i] which has smallest bit at h
-            //position[i] = header[i];
-        }
-        //node header0 = new node(null,-1,null);
-        int[] header0 = new int[n];
-        node now;
-        int num=0;//ans array's size
-        //System.out.println("before for loop");
-        for(int i=n-1;i>=0;i--){
-            int[] win = new int[n];//win[n] = array[i] win at some bit
-            //System.out.println("i= "+i);
-            for(int h=1;h<6;h++){
-                //System.out.println("h= "+h);
-                now = header[h];
-                is_bigest = 1;
-                while(now.next!=null){
-                    //System.out.println("now.next id= "+now.next.id);
-                    if(array[i][h]<=now.next.arr[h]){//if array[i] cant win now.next.arr
-                        //System.out.println("in if");
-                        position[h] = now;//last smaller than i
-                        is_bigest=0;//isnt biggest
-                        break;
+        System.out.println("---------------------------");*/
+        int[][] exist = new int[n][6];
+        exist[0] = array[n-1];
+        int num=1;//ans array's size
+        int win;
+        int check;
+        for(int i=n-2;i>=0;i--){//control array[i]
+            check=1;
+            for(int j=0;j<num;j++){//control exist[j]
+                win=0;
+                for(int h=1;h<6;h++){
+                    if(array[i][h]>exist[j][h]){
+                        win=1;
                     }
-                    win[now.next.id] = 1;//array[i] win now.next.arr
-                    now = now.next;
                 }
-                if(is_bigest==1) position[h] = now;
-                //System.out.println("position "+h+"'s id"+ position[h].id);
+                if(win==0){ check=0; break;}
             }
-            check=1;//if array[i] didnt lose all bit with any array
-            for(int j=0;j<num;j++){
-                if(win[header0[j]]<=0){
-                    check = 0;
-                    break;
-                }
-            }
-            /*now = header0;
-            //System.out.println("header0's id : "+ header0.id);
-            while(now.next!=null){
-                //System.out.println("now.next id= "+now.next.id);
-                if(win[now.next.id]<=0){//can be replace
-                    check=0;
-                    break;
-                }
-                now = now.next;
-            }*/
-
-            if(check == 1){//cant be replace
-                for(int h=1;h<6;h++){//insert in header[]
-                    node new_node = new node(array[i],i,position[h].next);
-                    position[h].next = new_node;
-                }
-                /*node new_node = new node(array[i],i,now.next);
-                now.next = new_node;//add it in the last of header0*/
-                header0[num] = i;
-                num++;//ans's number+1
-            }
-            /*//test---------------------------------------------
-            node test;
-            test = header0;
-            System.out.println("header 0");
-            while(test!=null){
-                System.out.println("id = "+test.id);
-                test = test.next;
-            }
-            for(int t=1;t<6;t++){
-                test = header[t];
-                System.out.println("header : "+t);
-                while(test!=null){
-                    System.out.println("id = "+test.id);
-                    test = test.next;
-                }
-            }
-            //test---------------------------------------------*/
+            if(check==1) exist[num++] = array[i];
         }
-        //System.out.println("num: "+num);
         int[][] ans = new int[num][6];
         int count = 0;
-        //now = header0.next;
         for(int i=num-1;i>=0;i--){
-            //System.out.println("now.id: "+now.id);
-            //System.out.println("now.arr: "+now.arr[0]);
-            ans[i] = array[header0[count++]];
-            //now = now.next;
+            ans[i] = exist[count++];
         }
         return ans;
     }
-    class node{
-        int[] arr;
-        int id;
-        node next;
-        public node(int[] arr,int id,node next){
-            this.arr = arr;
-            this.id = id;
-            this.next = next;
+    public static void sort(int[][] array){
+        int h=1;
+        while(h < n/3) h = 3*n+1;
+        //System.out.println("over h = "+h);
+        int[] temp = new int[6];
+        while(h>=1){
+            //System.out.println("h="+h);
+            for(int i=h;i<n;i++){
+                for(int j=i;j>=h && less(array[j],array[j-h]);j-=h){
+                    temp = array[j];
+                    array[j] = array[j-h];
+                    array[j-h] = temp;
+                }
+            }
+            h =h/3;
+        }
+        //System.out.println("over");
+    }
+    private static boolean less(int[] v,int[] w)
+    {
+        if(v[0]>w[0]){
+            return false;
+        }
+        else if(v[0]<w[0]){
+            return true;
+        }
+        else{
+            if(v[1]>w[1]){
+                return false;
+            }
+            else if(v[1]<w[1]){
+                return true;
+            }
+            else{
+                if(v[2]>w[2]){
+                    return false;
+                }
+                else if(v[2]<w[2]){
+                    return true;
+                }
+                else{
+                    if(v[3]>w[3]){
+                        return false;
+                    }
+                    else if(v[3]<w[3]){
+                        return true;
+                    }
+                    else{
+                        if(v[4]>w[4]){
+                            return false;
+                        }
+                        else if(v[4]<w[4]){
+                            return true;
+                        }
+                        else{
+                            if(v[5]>=w[5]){
+                                return false;
+                            }
+                            else{
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-    private static void sort(int[][] array,int lo,int hi){
+    /*private static void sort(int[][] array,int lo,int hi){
         if(hi<=lo) return;
         int mid = (hi+lo)>>1;
+        //System.out.println("lo = "+lo+" mid = "+mid+" hi = "+hi);
 
         sort(array,lo,mid);
         sort(array,mid+1,hi);
         merge(array,lo,mid,hi);
-    }
+    }*/
+    /*
     private static void merge(int[][] array,int lo,int mid,int hi){//array->x
-
+        //System.out.println("lo = "+lo+" hi = "+hi);
         System.arraycopy(array,lo,aux,lo,hi-lo+1);
-
+        System.out.println("aux--------------------------");
+        for(int i=lo;i<=hi;i++){
+            for(int j=0;j<6;j++){
+                System.out.print(aux[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------aux");
         int i=lo,j=mid+1;
         for(int k=lo;k<=hi;k++){
             if(i>mid){
@@ -149,6 +142,10 @@ public class HW08_4108056029_5 extends Buy_Phone_v2{
             }
             else{
                 for(int h=0;h<6;h++){
+                    //System.out.println("h = "+h);
+                    //System.out.print("aux["+i+"]["+h+"] : "+aux[i][h]);
+                    //System.out.println("v.s. aux["+j+"]["+h+"] : "+aux[j][h]);
+
                     if(aux[i][h]>aux[j][h]) {
                         array[k] = aux[j++];
                         break;
@@ -159,12 +156,14 @@ public class HW08_4108056029_5 extends Buy_Phone_v2{
                     }
                 }
             }
+
         }
-    }
-    public static void main(String[] args){
+
+    }*/
+    /*public static void main(String[] args){
         HW08_4108056029_5 test = new HW08_4108056029_5();
         //int[][] inputArr = {{2,3,5,0,1,2,3},{1,7,3,2,5,0,1},{3,0,0,2,3,4,7},{0,2,3,4,5,6,1},{2,2,5,6,7,1,0}};
-        int[][] inputArr = {{8,7,7,4,2,1},{2,4,9,2,2,1},{4,0,5,1,3,2},{5,2,4,3,7,3},{7,5,6,9,8,9},{8,1,1,1,1,1}};
+        int[][] inputArr = {{8,7,7,4,2,1},{2,4,9,2,2,1},{4,0,5,1,3,2},{5,2,4,3,7,3},{7,5,6,9,8,9}};
         int[][] ans = test.bestPhone(inputArr);
         for(int i=0;i<ans.length;i++){
             for(int j=0;j<6;j++){
@@ -172,5 +171,5 @@ public class HW08_4108056029_5 extends Buy_Phone_v2{
             }
             System.out.println();
         }
-    }
+    }*/
 }
