3870cabf3cadd99c4f8f1d5db66a3a77f6f50445
flora <flora90.1.10@gmail.com>
2021-05-05 17:03:37 +0800

    不傳入array

--- hw08/HW08_4108056029_3.java
+++ hw08/HW08_4108056029_3.java
@@ -1,81 +1,143 @@
 public class HW08_4108056029_3 extends Buy_Phone_v2{
-    static int[][] aux = new int[20000][16];
-    static int[] auxmark = new int[16];
+    static int[][] array;
+    final static public int[] array1 = new int[10000];
+    final static public int[] array2 = new int[10000];
+    final static public int[] array3 = new int[10000];
+    final static public int[] array4 = new int[10000];
+    final static public int[] array5 = new int[10000];
+    final static public int[][] aux = new int[10000][6];
+    //final static public int[][] aux = new int[10000][2];
+    static int[][] ans;
+    int n;
     public int[][] bestPhone(int[][] inputArr){
-        int n = inputArr.length;
-        int len = inputArr[0].length;
-        int[] mark = new int[n];
-        sort(inputArr,mark,0,n-1,0);
-        int[][] loser = new int[n][n];
+        //array = new int[inputArr.length][6];
+        //System.arraycopy(inputArr,0,array,0,inputArr.length);
+        array = inputArr;
+        n = array.length;
+        //this.array = array;
+        sort(0,n-1);
+        /*System.out.println("---------------------------");
         for(int i=0;i<n;i++){
-            mark[i] = i;
+            for(int j=0;j<6;j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
         }
-        for(int i=1;i<len;i++){
-            sort(inputArr,mark,0,n-1,i);
-            for(int j=0;j<n;j++){
-                for(int k=j;k>=0;k--){
-                    loser[mark[j]][mark[k]] = 1;
+        System.out.println("---------------------------");*/
+        int[][] exist = new int[n][6];
+        exist[0] = array[n-1];
+        int num=1;//ans array's size
+        int replace = 0;
+        //int check;
+        array1[0] = array[n-1][1];
+        array2[0] = array[n-1][2];
+        array3[0] = array[n-1][3];
+        array4[0] = array[n-1][4];
+        array5[0] = array[n-1][5];
+
+        for(int i=n-2;i>=0;i--){//control array[i]
+            replace =0;
+            for(int j=0;j<num;j++){//control exist[j]
+                if(array[i][1]>array1[j]){
+                }
+                else if(array[i][2]>array2[j]){
+                }
+                else if(array[i][3]>array3[j]){
+                }
+                else if(array[i][4]>array4[j]){
+                }
+                else if(array[i][5]>array5[j]){
+                }
+                else{
+                    replace=1;
+                    break;
                 }
             }
-        }
-        sort(inputArr,mark,0,n-1,0);
-        for(int j=0;j<n;j++){
-            for(int k=j;k>=0;k--){
-                loser[mark[j]][mark[k]] = 1;
+            if(replace<=0){
+                exist[num] = array[i];
+                array1[num] = array[i][1];
+                array2[num] = array[i][2];
+                array3[num] = array[i][3];
+                array4[num] = array[i][4];
+                array5[num++] = array[i][5];
             }
         }
-        int imp,count=0;
-        for(int k=0;k<n;k++){
-            imp = 1;
-            for(int j=0;j<n;j++){
-                if(loser[k][j]==0) imp = 0;
-            }
-            if(imp==1){
-                count++;
-            }
-            else mark[k]=-1;
-        }
-        int[][] ans = new int[count][len];
-        count=0;
-        for(int i=0;i<n;i++){
-            if(mark[i] > -1){
-                for(int j=0;j<len;j++){
-                    ans[count][j]  = inputArr[i][j];
-                }
-                count++;
-            }
+        int[][] ans = new int[num][6];
+        int count = 0;
+        for(int i=num-1;i>=0;i--){
+            ans[i] = exist[count++];
         }
         return ans;
     }
-    public static void sort(int[][] inputArr,int[] mark,int lo,int hi,int d){//inputArr->x
+    private static void sort(int lo,int hi){
         if(hi<=lo) return;
-        int [] count = new int[12];
-        int i;
-        for(i=lo;i<=hi;i++){
-            count[inputArr[i][d]+2]++;
-        }
-        for(i=1;i<11;i++){
-            count[i+1] += count[i];
-        }
-        for(i=lo;i<=hi;i++){
-            aux[count[inputArr[i][d]+1]] = inputArr[i];
-            auxmark[count[inputArr[i][d]+1]++] = mark[i];
-        }
-        for(i=lo;i<=hi;i++){
-            inputArr[i] = aux[i-lo];
-            mark[i] = auxmark[i-lo];
-        }
-        for(i=0;i<10;i++){
-            if(d==3) d=-1;
-            sort(inputArr,mark,lo+count[i],lo+count[i+1]-1,d+1);
-        }
+        int mid = (hi+lo)>>1;
+        //System.out.println("lo = "+lo+" mid = "+mid+" hi = "+hi);
+
+        sort(lo,mid);
+        sort(mid+1,hi);
+        merge(lo,mid,hi);
     }
-    public static void main(String[] args){
+    private static void merge(int lo,int mid,int hi){//array->x
+        //System.out.println("lo = "+lo+" hi = "+hi);
+        System.arraycopy(array,lo,aux,lo,hi-lo+1);
+        /*System.out.println("aux--------------------------");
+        for(int i=lo;i<=hi;i++){
+            for(int j=0;j<6;j++){
+                System.out.print(aux[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------aux");*/
+        int i=lo,j=mid+1;
+        for(int k=lo;k<=hi;k++){
+            if(i>mid){
+                array[k] = aux[j++];
+            }
+            else if(j>hi){
+                array[k] = aux[i++];
+            }
+            else{
+                for(int h=0;h<6;h++){
+                    //System.out.println("h = "+h);
+                    //System.out.print("aux["+i+"]["+h+"] : "+aux[i][h]);
+                    //System.out.println("v.s. aux["+j+"]["+h+"] : "+aux[j][h]);
+
+                    if(aux[i][h]>aux[j][h]) {
+                        array[k] = aux[j++];
+                        break;
+                    }
+                    else if(aux[i][h]<aux[j][h]){
+                        array[k] = aux[i++];
+                        break;
+                    }
+                }
+            }
+            /*System.out.print("array["+k+"]  : ");
+            for(int f=0;f<6;f++){
+                System.out.print(array[k][f]+" ");
+            }
+            System.out.println();*/
+        }
+        /*System.out.println("array--------------------------");
+        for(i=lo;i<=hi;i++){
+            for(j=0;j<6;j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------arr");*/
+    }
+    /*public static void main(String[] args){
         HW08_4108056029_3 test = new HW08_4108056029_3();
-        int[][] inputArr = {{2,4},{2,10},{5,4},{4,8},{1,1},{1,1},{5,5},{8,4},{10,2},{10,1}};
-        int[][] ans= test.bestPhone(inputArr);
-        /*for(int i=0;i<ans.length;i++){
-            System.out.println(ans[i][0]+" "+ans[i][1]);
-        }*/
-    }
+        //int[][] inputArr = {{2,3,5,0,1,2,3},{1,7,3,2,5,0,1},{3,0,0,2,3,4,7},{0,2,3,4,5,6,1},{2,2,5,6,7,1,0}};
+        int[][] inputArr = {{8,7,7,4,2,1},{2,4,9,2,2,1},{4,0,5,1,3,2},{5,2,4,3,7,3},{7,5,6,9,8,9}};
+        int[][] ans = test.bestPhone(inputArr);
+        for(int i=0;i<ans.length;i++){
+            for(int j=0;j<6;j++){
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }*/
 }
