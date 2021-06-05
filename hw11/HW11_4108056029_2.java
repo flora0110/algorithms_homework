555c1d2bbaca84d93fc80e64174fdb81f0e677b7
flora <flora90.1.10@gmail.com>
2021-06-06 05:59:59 +0800

    submit

--- hw11/HW11_4108056029_2.java
+++ hw11/HW11_4108056029_2.java
@@ -1,9 +1,9 @@
 public class HW11_4108056029_2 extends GroupCounting {
     public static void main(String[] args){
         HW11_4108056029_2 test = new HW11_4108056029_2();
-        String[] A = {"ABA","BAB","B","C","B","D","F","G"};
+        String[] A = {"ABA","BAB","BC","C","B","D","F","G"};
         String[] B = {"E","C","E","D","D","E","H","H"};
-        //System.out.println(test.count(A,B));
+        System.out.println(test.count(A,B));
     }
     public int count(String[] A, String[] B){
         int n=A.length;
@@ -24,15 +24,15 @@ public class HW11_4108056029_2 extends GroupCounting {
                 group_min++;
                 hashmap[key_a] = key_b;
                 hashmap[key_b] = -1;
-        //        System.out.println("mode 1 node_num: "+node_num+" group_min: "+group_min);
+            //    System.out.println("mode 1 node_num: "+node_num+" group_min: "+group_min);
             }
             else if(hashmap[key_a]==0) {
                 node_num++;
                 group_min++;
                 hashmap[key_a] = key_b;
-                while(hashmap[key_b]>0) key_a=hashmap[key_b];
+                while(hashmap[key_b]>0) key_b=hashmap[key_b];
                 hashmap[key_b]--;
-        //        System.out.println("mode 2 node_num: "+node_num+" group_min: "+group_min);
+            //    System.out.println("mode 2 node_num: "+node_num+" group_min: "+group_min);
             }
             else if(hashmap[key_b]==0) {
                 node_num++;
@@ -40,20 +40,20 @@ public class HW11_4108056029_2 extends GroupCounting {
                 hashmap[key_b] = key_a;
                 while(hashmap[key_a]>0) key_a=hashmap[key_a];
                 hashmap[key_a]--;
-        //        System.out.println("mode 3 node_num: "+node_num+" group_min: "+group_min);
+            //    System.out.println("mode 3 node_num: "+node_num+" group_min: "+group_min);
             }
             else{
                 while(hashmap[key_a]>0) {
                     //hashmap[key_a] =hashmap[hashmap[key_a]];
                     key_a=hashmap[key_a];
-        //            System.out.println("key_a = "+key_a);
+            //        System.out.println("key_a = "+key_a);
                 }
                 while(hashmap[key_b]>0) {
                     //hashmap[key_b] =hashmap[hashmap[key_b]];
                     key_b=hashmap[key_b];
-        //            System.out.println("key_b = "+key_b);
+            //        System.out.println("key_b = "+key_b);
                     }
-        //        System.out.println("key_a = "+key_a+" key_b = "+key_b);
+            //    System.out.println("key_a = "+key_a+" key_b = "+key_b);
                 if(key_a!=key_b){
                     group_min++;//link diff group => group-1
                     if(hashmap[key_a]<hashmap[key_b]){
@@ -65,7 +65,7 @@ public class HW11_4108056029_2 extends GroupCounting {
                         hashmap[key_a] = key_b;
                     }
                 }
-        //        System.out.println("mode 4 node_num: "+node_num+" group_min: "+group_min);
+            //    System.out.println("mode 4 node_num: "+node_num+" group_min: "+group_min);
             }
 		}
         return node_num-group_min;
