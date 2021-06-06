26f5d0dd11d57a66d366a92c2dee915bd2369f8a
flora <flora90.1.10@gmail.com>
2021-06-07 06:21:30 +0800

    fix key error(cap-1),()

--- hw11/HW11_4108056029_2.java
+++ hw11/HW11_4108056029_2.java
@@ -1,71 +1,181 @@
+//parent[], child[], parent need initial, path compression
+//hashmap[key]==null -> parent[key]==0
+//len = cap<<1+1;
 public class HW11_4108056029_2 extends GroupCounting {
     public static void main(String[] args){
         HW11_4108056029_2 test = new HW11_4108056029_2();
-        String[] A = {"ABA","BAB","BC","C","B","D","F","G"};
-        String[] B = {"E","C","E","D","D","E","H","H"};
+        String[] A = {"CCCCC","BAB","BC","C","B","D","F","G","B","B"};
+        String[] B = {"z","C","E","D","D","E","H","H","H","qqq"};
+        //String[] A = {"A","e","A","A"};
+        //String[] B = {"B","f","e","g"};
+        //String[] A = {"C","e","C","C","C","C"};
+        //String[] B = {"B","f","e","g","J","B"};
         System.out.println(test.count(A,B));
+    }
+    class Head {
+        String str;
+        int index;
+        Head next_head;//same key
+        public Head(String str){
+            this.str = str;
+        }
     }
     public int count(String[] A, String[] B){
         int n=A.length;
-        int cap = ((1<<(int)(Math.log(n+1)/Math.log(2)+1)))*((1<<(int)(Math.log(n+1)/Math.log(2)+1)))-1;
-        //System.out.println("cap: "+cap+" n:"+n);
-        int[] hashmap = new int[cap+1];
+        int cap = (1<<(int)(Math.log(n+1)/Math.log(2)+1))-1;
+        System.out.println("cap: "+cap);
+        int len = cap<<1+1;
+        Head[] hashmap = new Head[len];
+        int[] parent = new int[len];//0: this node not exsit, 1~n: n's child
+        int[] child = new int[len];
+
         int key_a,key_b;
-        int node_num=0,group_min=0;
+        int node_num=0,group_min=0; //node_num-group_min==group num
 		for (int i=0;i<n;i++) {
-            key_a=(A[i].hashCode() & 0x7fffffff) & (cap-1) +1;
-            key_b=(B[i].hashCode() & 0x7fffffff) & (cap-1) +1;
-            System.out.println("\nA: "+A[i]+" code: "+(A[i].hashCode() & 0x7fffffff));
-            System.out.println("B: "+B[i]+" code: "+(B[i].hashCode() & 0x7fffffff));
-            System.out.println("A :"+A[i]+" key_a = "+key_a);
-            System.out.println("B :"+B[i]+" key_b = "+key_b);
-            if(hashmap[key_a]==0 && hashmap[key_b]==0){
-                node_num+=2;
-                group_min++;
-                hashmap[key_a] = key_b;
-                hashmap[key_b] = -1;
-                System.out.println("mode 1 node_num: "+node_num+" group_min: "+group_min);
+            key_a=((A[i].hashCode() & 0x7fffffff)& (cap))+1;//key range 1~cap
+            key_b=((B[i].hashCode() & 0x7fffffff)& (cap))+1;
+            System.out.println((A[i].hashCode() & 0x7fffffff));
+            System.out.println(((A[i].hashCode() & 0x7fffffff)& (cap)));
+            System.out.println();
+            if(parent[key_a]==0 && parent[key_b]==0){//both not in map //-> parent[key]==0
+                node_num+=2;//num of node +2
+                group_min++;//two node(two group) link -> num of group -1
+                hashmap[key_a] = new Head(A[i]);
+                hashmap[key_b] = new Head(B[i]);
+                parent[key_a] = key_b;//link A[i] to B[i]
+                parent[key_b] = key_b;//initial
+                child[key_b]=1;//node B[i] has one child
             }
-            else if(hashmap[key_a]==0) {
+            else if(parent[key_a]==0) {//key_a's place is empty -> A[i] is not in map
+                node_num++;//num of node +1
+                group_min++;//link a node to a group == link two group -> num of group-1
+                hashmap[key_a] = new Head(A[i]);
+
+                //get B[i]'s head's root
+                //find b's Head's index
+                int index_of_b;
+                if(hashmap[key_b].str.equals(B[i])) index_of_b=key_b;
+                else{
+                    Head head_of_b = hashmap[key_b];
+                    while(true){
+                        if(head_of_b.next_head==null){//head_of_b now is point at tail
+                            head_of_b.next_head = new Head(B[i]);       //B[i] is not in map ,num of node +1
+                            head_of_b.next_head.index=cap+(++node_num);//this index is crush -> give B[i] a new index
+                            index_of_b = cap+node_num;
+                            parent[index_of_b]=index_of_b;//initial
+                            break;
+                        }
+                        head_of_b = head_of_b.next_head;
+                        if(head_of_b.str.equals(B[i])) {
+                            index_of_b=head_of_b.index;//head_of_b is B[i]'s Head, its index is index_of_b
+                            break;
+                        }
+                    }
+                }
+                //find B[i]'s root
+                while(parent[index_of_b]!=index_of_b){//index_of_b has parent == is not root
+                    parent[index_of_b] = parent[parent[index_of_b]];
+                    index_of_b = parent[index_of_b];
+                }
+                parent[key_a] = index_of_b;
+                child[index_of_b]++;
+            }
+            else if(parent[key_b]==0) {//B[i] is not in map
                 node_num++;
                 group_min++;
-                while(hashmap[key_b]>0) key_b=hashmap[key_b];
-                hashmap[key_a] = key_b;
-                hashmap[key_b]--;
-                System.out.println("mode 2 node_num: "+node_num+" group_min: "+group_min);
-            }
-            else if(hashmap[key_b]==0) {
-                node_num++;
-                group_min++;
-                while(hashmap[key_a]>0) key_a=hashmap[key_a];
-                hashmap[key_b] = key_a;
-                hashmap[key_a]--;
-                System.out.println("mode 3 node_num: "+node_num+" group_min: "+group_min);
+                hashmap[key_b] = new Head(B[i]);
+
+                //get a's head's root
+                //find a's Head's index
+                int index_of_a;
+                if(hashmap[key_a].str.equals(A[i])) index_of_a=key_a;
+                else{
+                    Head head_of_a = hashmap[key_a];
+                    while(true){
+                        if(head_of_a.next_head==null){//head_of_a now is point at tail
+                            head_of_a.next_head = new Head(A[i]);
+                            head_of_a.next_head.index=cap+(++node_num);
+                            index_of_a=cap+node_num;
+                            parent[index_of_a]=index_of_a;//initial
+                            break;
+                        }
+                        head_of_a = head_of_a.next_head;
+                        if(head_of_a.str.equals(A[i])) {
+                            index_of_a=head_of_a.index;
+                            break;
+                        }
+                    }
+                }
+                while(parent[index_of_a]!=index_of_a){//index_of_a has parent == index_of_a is not root
+                    parent[index_of_a] = parent[parent[index_of_a]];
+                    index_of_a = parent[index_of_a];
+                }
+                parent[key_b] = index_of_a;//link b to a
+                child[index_of_a]++;//a's child+1
             }
             else{
-                while(hashmap[key_a]>0) {
-                    //hashmap[key_a] =hashmap[hashmap[key_a]];
-                    key_a=hashmap[key_a];
-                    System.out.println("key_a = "+key_a);
-                }
-                while(hashmap[key_b]>0) {
-                    //hashmap[key_b] =hashmap[hashmap[key_b]];
-                    key_b=hashmap[key_b];
-                    System.out.println("key_b = "+key_b);
+                //get A[i]'s head 's root
+                //find a's Head's index
+                int index_of_a;
+                if(hashmap[key_a].str.equals(A[i])) index_of_a=key_a;
+                else{
+                    Head head_of_a = hashmap[key_a];
+                    while(true){
+                        if(head_of_a.next_head==null){//head_of_a now is point at tail
+                            head_of_a.next_head = new Head(A[i]);
+                            head_of_a.next_head.index=cap+(++node_num);
+                            index_of_a=cap+node_num;
+                            parent[index_of_a]=index_of_a;//initial
+                            break;
+                        }
+                        head_of_a = head_of_a.next_head;
+                        if(head_of_a.str.equals(A[i])) {
+                            index_of_a=head_of_a.index;
+                            break;
+                        }
                     }
-                System.out.println("key_a = "+key_a+" key_b = "+key_b);
-                if(key_a!=key_b){
-                    group_min++;//link diff group => group-1
-                    if(hashmap[key_a]<hashmap[key_b]){
-                        hashmap[key_a] +=(hashmap[key_b]-1);
-                        hashmap[key_b] = key_a;
+                }
+                while(parent[index_of_a]!=index_of_a){//index_of_a has parent == index_of_a is not root
+                    parent[index_of_a] = parent[parent[index_of_a]];
+                    index_of_a = parent[index_of_a];
+                }
+                //get B[i]'s head 's root
+                //find b's Head's index
+                int index_of_b;
+                if(hashmap[key_b].str.equals(B[i])) index_of_b=key_b;
+                else{
+                    Head head_of_b = hashmap[key_b];
+                    while(true){
+                        if(head_of_b.next_head==null){//head_of_b now is point at tail
+                            head_of_b.next_head = new Head(B[i]);
+                            head_of_b.next_head.index=cap+(++node_num);
+                            index_of_b=cap+node_num;
+                            parent[index_of_b]=index_of_b;//initial
+                            break;
+                        }
+                        head_of_b = head_of_b.next_head;
+                        if(head_of_b.str.equals(B[i])) {
+                            index_of_b=head_of_b.index;
+                            break;
+                        }
+                    }
+                }
+                while(parent[index_of_b]!=index_of_b){//index_of_b has parent == index_of_b is not root
+                    parent[index_of_b] = parent[parent[index_of_b]];
+                    index_of_b = parent[index_of_b];
+                }
+
+                if(index_of_a!=index_of_b){//not same group
+                    if(child[index_of_a]>child[index_of_b]){//weighted
+                        child[index_of_a]+=(child[index_of_b]+1);
+                        parent[index_of_b] = index_of_a;
                     }
                     else{
-                        hashmap[key_b] +=(hashmap[key_a]-1);
-                        hashmap[key_a] = key_b;
+                        child[index_of_b]+=(child[index_of_a]+1);
+                        parent[index_of_a] = index_of_b;
                     }
+                    group_min++;//link two group -> num of group-1
                 }
-                System.out.println("mode 4 node_num: "+node_num+" group_min: "+group_min);
             }
 		}
         return node_num-group_min;
